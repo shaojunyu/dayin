119aa86327e405a28c00744a40286fba503c1e9e
yushaojun <968538@163.com>
2016-05-08 13:12:32 +0800

    no message

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -7,18 +7,19 @@
  */
 class Test extends CI_Controller{
     public function index(){
-        $this->load->model('user_model');
-        $user = new User_model();
-
-        $this->db->where('username','123');
-        $r = $this->db->get('user');
-        var_dump($r->result_array());
-
-        var_dump($user->login('233','123456'));
-
-        //$user->signup('123456','123456','华中科技大学');
-        echo $user->login('123456','123456');
-        $user->userInfo2session('123456');
-        var_dump($this->session->userdata());
+//      $this->load->model('user_model');
+//      $user = new User_model();
+//
+//      $this->db->where('username','123');
+//      $r = $this->db->get('user');
+//      var_dump($r->result_array());
+//
+//      var_dump($user->login('233','123456'));
+//
+//      //$user->signup('123456','123456','华中科技大学');
+//      echo $user->login('123456','123456');
+//      $user->userInfo2session('123456');
+//      var_dump($this->session->userdata());
+	$this->load->view('welcome_message');
     }
 }
