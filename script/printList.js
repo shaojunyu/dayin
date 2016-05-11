21635cddbd6b7ebf4cb98f324afd2c3fda290eed
hanrenguang <413526182@qq.com>
2016-05-12 05:00:04 +0800

    提交后跳转

--- script/printList.js
+++ script/printList.js
@@ -349,7 +349,7 @@ function submitOrder(Data) {
 	    data:JSON.stringify(Data),
 	    success:function(data) {
 	    	if(data.success) {
-				//location.href = "myself";
+				location.href = "myself";
 	    	}
 	   		else {
 	        	showError(data.msg);
