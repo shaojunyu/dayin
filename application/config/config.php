f5c75c972d14d75c886846d15700474a48e256a8
yushaojun <968538@163.com>
2016-08-26 22:36:37 +0800

    no message

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'www.99dayin.com';
+$config['base_url'] = 'http://dayin.4nian.cc/';
 
 /*
 |--------------------------------------------------------------------------
