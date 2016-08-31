691d9e382c8e013d9351c9ae49695531656e7144
yushaojun <968538@163.com>
2016-09-01 04:31:59 +0800

    no message

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-//$config['base_url'] = 'http://dayin.4nian.cc/';
+$config['base_url'] = 'http://dayin.4nian.cc/';
 
 /*
 |--------------------------------------------------------------------------
